87354b26469940c6371af4a17134c2671acc253a
shivangipatodiya <shivu2610@gmail.com>
2021-09-23 23:54:24 +0000

    tests added

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,14 +8,20 @@ const request = require('request');
   
 // })
 
-const arg = process.argv.slice(2);
-request('https://api.thecatapi.com/v1/breeds/search?q=' + arg, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    return console.log('Breed Not Found');
-  }
-  console.log(data[0].description);
-});
+
+const fetchBreedDescription = (breedName, cb) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      return cb(error);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return cb('Breed Not Found');
+    }
+    cb(null, data[0].description);
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
